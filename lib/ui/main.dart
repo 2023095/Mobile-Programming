97e62c227b700c535270c67bc8843fc82b564625
Octowiaz <octavius0044@gmail.com>
2026-02-26 13:27:40 +0800

    1

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lesson_02/widgets_layout_demo.dart';
-import 'dart_demo_screen.dart';
+import 'package:lesson_02/ui/2_widget_layout/widgets_layout_demo.dart';
+import 'package:lesson_02/ui/3_state_management/state_management_demo.dart';
+import '1_dart/dart_demo_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -43,6 +44,7 @@ class HomeScreen extends StatelessWidget {
             },
           ),
 
+
           ListTile(
             title: const Text("2. Widgets and layout"),
             leading: const Icon(Icons.code),
@@ -51,6 +53,20 @@ class HomeScreen extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) => const WidgetsLayoutDemo(),
+                ),
+              );
+            },
+          ),
+
+
+          ListTile(
+            title: const Text("3. State Management"),
+            leading: const Icon(Icons.code),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const StateManagementDemo(),
                 ),
               );
             },
